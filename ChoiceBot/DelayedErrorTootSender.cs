e935e42b114df45c5f3ed1f43f8ff14babcd1fea
Ch <ccwpc@hanmail.net>
2021-06-14 01:02:17 +0900

    add error message delaying (untested)

---
+++ ChoiceBot/DelayedErrorTootSender.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mastonet;
+
+namespace ChoiceBot
+{
+    public class DelayedErrorTootSender
+    {
+        private DateTime _errorTootTimeLast = DateTime.MinValue;
+        /**
+         * Zero means this is first time shooting error after enough delay
+         */
+        private static   TimeSpan _errorTootIntervalNow = TimeSpan.Zero;
+        public TimeSpan ErrorTootIntervalStart { get; set; } = TimeSpan.FromSeconds(30);
+        public TimeSpan ErrorTootIntervalMax { get; set; } = TimeSpan.FromHours(1);
+        public TimeSpan ErrorTootIntervalResetSpan { get; set; } = TimeSpan.FromSeconds(30);
+        
+        private readonly string _exceptionMessageHead = "@sftblw@twingyeo.kr\r\n[!] 예외가 발생하였습니다.";
+        private bool _sendErrorMessage = true;
+
+        public DelayedErrorTootSender() { }
+
+        public DelayedErrorTootSender(string exceptionMessageHead, bool sendErrorMessage = true)
+        {
+            _exceptionMessageHead = exceptionMessageHead;
+            _sendErrorMessage = sendErrorMessage;
+        }
+        
+        
+        public async Task SendLogTootDelayed(UnhandledExceptionEventArgs e, MastodonClient client)
+        {
+            if (_errorTootTimeLast + _errorTootIntervalNow >= DateTime.Now) { return; }
+            
+            _errorTootTimeLast = DateTime.Now;
+
+            UpdateInterval();
+
+            await SendLogToot(e, client);
+        }
+
+        private void UpdateInterval()
+        {
+            if (_errorTootIntervalNow < ErrorTootIntervalStart)
+            {
+                _errorTootIntervalNow = ErrorTootIntervalStart;
+                return;
+            }
+            
+            var newInterval = _errorTootIntervalNow.Multiply(2);
+            if (DateTime.Now < _errorTootTimeLast + newInterval)
+            {
+                _errorTootIntervalNow =
+                    (newInterval < ErrorTootIntervalMax)
+                        ? newInterval
+                        : ErrorTootIntervalMax;
+            }
+            else
+            {
+                _errorTootIntervalNow = TimeSpan.Zero;
+            }
+        }
+
+        private async Task SendLogToot(UnhandledExceptionEventArgs e, MastodonClient client)
+        {
+            string message = _exceptionMessageHead + "\r\n" + ((e.ExceptionObject as Exception)?.Message ?? e.ToString());
+            message = message[..Math.Max((message.Length - 1), 450)];
+            
+            await client.PostStatus(message, Visibility.Private);
+        }
+    }
+}
--- ChoiceBot/Program.cs
+++ ChoiceBot/Program.cs
@@ -10,7 +10,7 @@ namespace ChoiceBot
     public static class Program
     {
         private static MastodonClient? _client;
-        private const string ExceptionMessage = "[!] 예외가 발생하였습니다.\r\n@sftblw@twingyeo.kr";
+        private static DelayedErrorTootSender _errorTooter = new DelayedErrorTootSender();
 
         public static void Main(string[] args)
         {
@@ -21,7 +21,11 @@ namespace ChoiceBot
         private static async void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             Console.Error.WriteLine(e);
-            await _client!.PostStatus(ExceptionMessage, Visibility.Unlisted);
+
+            if (_client != null)
+            {
+                await _errorTooter.SendLogTootDelayed(e, _client);
+            }
         }
 
         private static async Task Execute()
